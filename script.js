0ee0f590e801ab156d0a355178076799a4f74c11
Loperaus <161947726+Loperaus@users.noreply.github.com>
2024-08-06 13:16:06 +1100

    Update script.js

--- script.js
+++ script.js
@@ -7,24 +7,22 @@ const slideCount = slides.length;
 let slideIndex = 0;
 
 
-// Устанавливаем обработчики событий для кнопок
 prevButton.addEventListener('click', showPreviousSlide);
 nextButton.addEventListener('click', showNextSlide);
 
 
-// Функция для показа предыдущего слайда
 function showPreviousSlide() {
   slideIndex = (slideIndex - 1 + slideCount) % slideCount;
   updateSlider();
 }
 
-// Функция для показа следующего слайда
+
 function showNextSlide() {
   slideIndex = (slideIndex + 1) % slideCount;
   updateSlider();
 }
 
-// Функция для обновления отображения слайдера
+
 function updateSlider() {
   slides.forEach((slide, index) => {
     if (index === slideIndex) {
@@ -35,7 +33,5 @@ function updateSlider() {
   });
 }
 
-
-// Инициализация слайдера
 updateSlider();
 
